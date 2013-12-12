85d9f9aed4dd40381f58ec463ae85e3ff017f56f
mtkanda@hawaii.edu <mtkanda@hawaii.edu>
2013-12-12 10:08:26 -1000

    replaced missing files

---
+++ Final_Project/newUser.php
@@ -0,0 +1,81 @@
+<?php
+	require 'connect.php';
+	$userExistance = false;
+	
+	$number_of_users = mysqli_fetch_array($mysqli -> query("SELECT COUNT(username) FROM user_info"));
+	
+	if(isset($_POST["email"]) && 
+	   isset($_POST["password"])){
+	   
+	   $email = $_POST["email"];
+	   $password = $_POST["password"];
+	   $street_address = $_POST["street_address"];
+	   $city = $_POST["city"];
+	   $state = $_POST["state"];
+	   $zip = $_POST["zip"];
+	   $country = $_POST["country"];
+	   
+	   $query_run = $mysqli -> query("SELECT COUNT(username)  
+									  FROM user_info 
+									  WHERE username = '$email'");
+									  
+	   $query_num_rows = mysqli_fetch_array($query_run);
+	   
+	   if($query_num_rows[0] > 0){
+			$userExistance = true;
+	   }
+	   else{						  
+			$mysqli -> query("INSERT INTO `user_info`(`username`, 
+												  `password`, 
+												  `userid`,
+												  `street_address`,
+												  `city`,
+												  `state`,
+												  `zip`,
+												  `country`) 
+						  VALUES ('$email',
+								  '$password',
+								   $number_of_users[0] + 1,
+								  '$street_address',
+								  '$city',
+								  '$state',
+								  '$zip',
+								  '$country')");
+								  
+			header("Location: final_project_login.php");
+		}
+	}
+?>
+<html>
+	<head>
+		<title>Login Page</title>
+		<link href = "newUser.css" rel = "stylesheet" type = "text/css">
+	</head>
+	<body>
+		<div id = "container">
+			<div id = "title">
+				<h2>Account Information<h2>
+			</div>
+			<div id = "newUserForm">
+				<form name = "final_project_newUser" action = "newUser.php" onsubmit = "return validateForm()" method = "POST">
+					<br>E-mail: <input id = "inputEmail" type = "email" name = "email">
+					<br>Password: <input id = "inputPassword" type = "password" name = "password">
+					<br> Confirm Password: <input id = "confirmPassword" type = "password" name = "confirm_password">
+					<br> Street Address: <input id = "streetAddress" type = "text" name = "street_address">
+					<br> City: <input id = "city" type = "text" name = "city">
+					<br> State: <input id = "state" type = "text" name = "state">
+					<br> Zip: <input id = "zip" type = "text" name = "zip">
+					<br> Country: <input id = "country" type = "text" name = "country">
+					<br><input type = "submit" value = "Submit">
+				</form>
+				
+				<script src = "new_user_script.js"></script>
+			</div>
+		</div>
+		<?php
+			if($userExistance == true){
+				echo '<script type="text/javascript"> alert("Sorry! There already an account by that username") </script>';
+			}
+		?>
+	</body>
+</head>
